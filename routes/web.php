e956f2aa5dcfffab1a64e917bc7aab24a0265343
SvenKeimpema <keimpje1234@gmail.com>
2025-03-10 10:25:10 +0100

    fix: better validation for matchmaking process

--- routes/web.php
+++ routes/web.php
@@ -7,18 +7,27 @@ use Inertia\Inertia;
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/', function () {
+        $game = GameUser::where("user_id", auth()->id())->first();
+        if($game) {
+            // we are unsure here if there is a user already connected to the game so we redirect to the matchmaking page which does this validation for us
+            return redirect()->route('matchmaking');
+        }
         return Inertia::render('welcome');
     })->name('home');
     Route::get('/play/online', function () {
         return Inertia::render('matchmaking');
-    });
+    })->name('matchmaking');
     Route::get('/play/match', function () {
         $game = GameUser::where("user_id", auth()->id())->first();
-        if($game) {
-            return Inertia::render('game');
-        }
-        return redirect()->route('home');
-    });
+        if(!$game) return redirect()->route('home');
+
+        $game = GameUser::where("game_id", $game->game_id)->get();
+
+        if($game->count() < 2) return redirect()->route('matchmaking');
+
+        return Inertia::render('game');
+
+    })->name('game.play');
     Route::post('/game/matchmake', [GameController::class, "matchmake"])->name('game.join');
     Route::post('/game/found', [GameController::class, "found"])->name('game.found');
     Route::post('/game/users', [GameController::class, "userNames"])->name('game.users');
